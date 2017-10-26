63a2509fc2b149a2794a3e757d5e3b113ef36f85
subpanda101 <pokemonbrandon1234@gmail.com>
2017-10-26 20:24:56 +0100

    File handling and RSS entry.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,28 @@
 import discord
 import asyncio
-import time
+import feedparser
 
 client = discord.Client()
 
 
 async def my_background_task():
     await client.wait_until_ready()
-    day = time.strftime("%a")
-    hour = time.strftime("%H")
-    channel = discord.Object(id='369587962603896834')
-    if day == "Fri" and hour == "15":
-        await client.send_message(channel, "The new episode is here! @botpings")
+    # Defining the feed, Parsing the feed and opening the text file.
+
+    shoujo_shuumatsu_feed = "https://nyaa.si/?page=rss&q=shoujo+shuumatsu+1080p+horriblesubs&c=0_0&f=0"
+    feed = feedparser.parse(shoujo_shuumatsu_feed)
+    file = open("episode_count", "r+")
+    x = len(feed["items"])
+    current_amount = int(file.read())
+
+    if x > current_amount:
+        channel = discord.Object(id='369587962603896834')
+        await client.send_message(channel, "{0}".format("The new episode is here! @botpings"))
+        current_amount = current_amount + 1
+        file.seek(0)
+        file.write(str(current_amount))
+        file.truncate()
+        file.close()
         await asyncio.sleep(60)  # task runs every 60 seconds
 
 
