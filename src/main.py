2b923610fe961391bdbfbd0f09d64996fed6e838
subpanda101 <pokemonbrandon1234@gmail.com>
2017-10-28 15:28:02 +0100

    The bot actually works now!

--- src/main.py
+++ src/main.py
@@ -7,23 +7,26 @@ client = discord.Client()
 
 async def my_background_task():
     await client.wait_until_ready()
-    # Defining the feed, Parsing the feed and opening the text file.
+    channel = discord.Object(id='373836665937985539')
 
-    shoujo_shuumatsu_feed = "https://nyaa.si/?page=rss&q=shoujo+shuumatsu+1080p+horriblesubs&c=0_0&f=0"
-    feed = feedparser.parse(shoujo_shuumatsu_feed)
-    file = open("episode_count", "r+")
-    x = len(feed["items"])
-    current_amount = int(file.read())
+    while not client.is_closed:
+        shoujo_shuumatsu_feed = "https://nyaa.si/?page=rss&q=shoujo+shuumatsu+1080p+horriblesubs&c=0_0&f=0"
+        feed = feedparser.parse(shoujo_shuumatsu_feed)
+        file = open("episode_count", "r+")
+        x = len(feed["items"])
+        current_amount = int(file.read())
 
-    if x > current_amount:
-        channel = discord.Object(id='369587962603896834')
-        await client.send_message(channel, "{0}".format("The new episode is here! @botpings"))
-        current_amount = current_amount + 1
-        file.seek(0)
-        file.write(str(current_amount))
-        file.truncate()
-        file.close()
-        await asyncio.sleep(1800)  # task runs every 60 seconds
+        if x > current_amount:
+            await client.send_message(channel, "{0}".format("The new episode is here! @botpings"))
+            current_amount = current_amount + 1
+            file.seek(0)
+            file.write(str(current_amount))
+            file.truncate()
+            file.close()
+            await asyncio.sleep(900)  # task runs every 60 seconds
+        else:
+            print("There is no new episode")
+            await asyncio.sleep(900)  # task runs every 60 seconds
 
 
 @client.event
@@ -32,6 +35,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
 
 client.loop.create_task(my_background_task())
 client.run('MzY5NTg1NTE3MzYyMTUxNDM2.DMawKA.wTHjFgny7JZx6Y1uAsJAFbEX5F4')
