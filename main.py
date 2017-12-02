73793e4ec05dd9683a5ebf1121ff5540460dd51f
AKSoo <AKSoo@users.noreply.github.com>
2017-12-02 15:37:18 +0900

    tkmiz art-media

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ client = discord.Client()
 
 async def update_checks():
     await client.wait_until_ready()
-    episode_count = 0
+    episode_count = None
+    tkmiz_last_published = None
 
     while not client.is_closed:
-        episode_count = await new_episode_check(episode_count)
-
+        [episode_count, tkmiz_last_published] = await asyncio.gather( \
+                new_episode_check(episode_count), \
+                tkmiz_media_check(tkmiz_last_published))
         await asyncio.sleep(900)
 
 
@@ -23,12 +25,25 @@ async def new_episode_check(current_count):
     feed = feedparser.parse(shoujo_shuumatsu_feed)
 
     count = len(feed['items'])
-    if current_count > 0 and count > current_count:
+    if current_count is not None and count > current_count:
         link = feed['items'][0]['link']
         await client.send_message(episode_announcements, \
                 "The new episode is here! {0} \n The link is here! {1}".format(botpings.mention, link))
 
     return count
+
+
+async def tkmiz_media_check(last_published):
+    art_media = client.get_channel(id='351101813212184577')
+    tkmiz_media_feed = "https://twitrss.me/twitter_user_to_rss/?user=tkmiz%2Fmedia"
+    feed = feedparser.parse(tkmiz_media_feed)
+
+    published = feed['items'][0].published_parsed
+    if last_published is not None and published > last_published:
+        link = feed['items'][0]['link']
+        await client.send_message(art_media, "Fresh potato from tsukumizu! \n {0}".format(link))
+
+    return published
 
 
 @client.event
